94356ca7692d888b261cbacc15247127108206ae
Nolan Knill <nolanknill@gmail.com>
2022-10-27 15:03:31 -0400

    Lecture: Classes demo code with parent-child classes and using methods within HOF

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
-console.log("You are in the right place!");
+class Monster {
+    constructor(name, health, move = "Scratch") {
+        this.name = name;
+        this.health = health;
+        this.move = move;
+        this.misunderstood = true;
+    }
 
-function Monster(name, health, move = "Scratch") {
-    this.name = name;
-    this.health = health;
-    this.move = move;
-
-    this.attack = function() {
+    attack() {
         // output example: Pikachu used "Thunder"!
         console.log(`${this.name} used "${this.move}"`);
     }
 }
 
-function Pokemon(name, health, move, type) {
-    Monster.call(this, name, health, move);
-    this.type = type;
-    this.level = 1;
+class Dragon extends Monster {
+    // Possible dragon properties: canFly, size
+    constructor(name, health, move, rider) {
+        super(name, health, move);
+        
+        this.rider = rider;
+    }
+
+    attack() {
+        console.log(`${this.rider} tells ${this.name} to attack!`);
+        super.attack();
+    }
 }
 
-Monster.prototype.misunderstood = true;
-Pokemon.prototype.levelUp = function() {
-    this.level++;
+class Pokemon extends Monster {  
+    level = 1;  
+    
+    constructor(name, health, move, type) {
+        super(name, health, move);
 
-    this.health += 3;
+        this.type = type;
+    }
 
-    console.log(`${this.name} has levelled up! They are now level ${this.level} with health ${this.health}`);
+    levelUp() {
+        this.level++;
+        this.health += 3;
+
+        console.log(`${this.name} has levelled up! They are now level ${this.level} with health ${this.health}`);
+    }
 }
 
-Object.setPrototypeOf(Pokemon.prototype, Monster.prototype);
+const godzilla = new Monster("Godzilla", 9001, "Stomp");
+const pikachu = new Pokemon("Pikachu", 25, "Thunder", "Electric");
+const viserion = new Dragon("Viserion", 25000, "Blue Fire", "Night King");
+
+/* When button is clicked -> level up Pikachu */
+const buttonEl = document.querySelector("button");
+buttonEl.addEventListener("click", function() { 
+    pikachu.levelUp(); 
+});
+
+viserion.attack();
+godzilla.attack();
+// Doesn't work
+// godzilla.levelUp();
+pikachu.attack();
+pikachu.levelUp();
